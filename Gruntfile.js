d3c5b789f5666c8b5e49b665393047e6bb15db3e
Christophe Coquelet <coquelet.c@gmail.com>
2016-04-23 00:11:11 +0200

    add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var path = require('path');
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+module.exports = function (grunt) {
+    require('time-grunt');
+    require('jit-grunt');
+    
+    var configPath = path.resolve('./grunt/config');
+    var taskPath   = path.resolve('./grunt/tasks');
+    
+    fs.readdirSync(configPath).forEach(function (filename) {
+        if (endsWith(filename, '.js')) {
+            require(path.join(configPath, filename))(grunt);
+        }
+    });
+    
+    fs.readdirSync(taskPath).forEach(function (filename) {
+        if (endsWith(filename, '.js')) {
+            require(path.join(taskPath, filename))(grunt);
+        }
+    });
+};
