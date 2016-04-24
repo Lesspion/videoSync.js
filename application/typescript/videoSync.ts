4b4779671e1944d34eda823a778e6562f46b0a55
Christophe Coquelet <coquelet.c@gmail.com>
2016-04-24 19:23:54 +0200

    add constructor

--- application/typescript/videoSync.ts
+++ application/typescript/videoSync.ts
@@ -1,13 +1,39 @@
 class Video
 {
-    node: Node;
-    instance: Video;
-    
-    constructor(node?: Node) {
-        if (this.instance !== null) {
-            this.node = node;
-            this.instance = this;
-        }    
-        return this.instance;
-    }
+	nodeElem : HTMLVideoElement;
+	onPlay : Boolean = false;
+	that : Video = this;
+	
+	constructor(nodeELem : HTMLVideoElement | string) {
+		if (typeof nodeELem === "string") {
+			this.nodeElem = (<HTMLVideoElement>document.querySelector(nodeELem));
+		} else {
+			this.nodeElem = nodeELem;
+		}
+		
+		this.nodeElem.addEventListener('click', function () {
+			if (this.paused) {
+				this.play();
+			} else {
+				this.pause();
+			}
+		});
+	}
+	
+	setHeight(h: number) : void {
+		this.nodeElem.height = h;
+	}
+	
+	setWidth(w: number) : void {
+		this.nodeElem.width = w;
+	}
+	
+	setSize(sizeObject: any) {
+		this.setHeight(sizeObject.h || sizeObject.height);
+		this.setWidth(sizeObject.w || sizeObject.width);
+	}
+	
+	getNode() : HTMLVideoElement {
+		return this.nodeElem;
+	}
 }
